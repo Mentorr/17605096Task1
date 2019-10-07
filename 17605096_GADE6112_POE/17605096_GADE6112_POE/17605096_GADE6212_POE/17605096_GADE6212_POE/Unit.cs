970ff39bc88eacc042786a82c279a77a152c47ef
Mentorr <43600054+Mentorr@users.noreply.github.com>
2019-10-07 14:59:00 +0200

    Add files via upload

---
+++ 17605096_GADE6112_POE/17605096_GADE6112_POE/17605096_GADE6212_POE/17605096_GADE6212_POE/Unit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17605096_GADE6212_POE
+{
+    abstract class Unit
+    {
+    }
+}
